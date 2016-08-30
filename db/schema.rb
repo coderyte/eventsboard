26b6d446a52b3e8922d4465b46ded4ed9d8192ed
coderyte <teamcoderyte@gmail.com>
2016-08-30 03:21:07 +0000

    Add strong parameter to devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160830012908) do
+ActiveRecord::Schema.define(version: 20160830024129) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -40,6 +40,10 @@ ActiveRecord::Schema.define(version: 20160830012908) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
+    t.string   "organization_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
