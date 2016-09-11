2a669e7163157b75cf17979269733eafbfdece87
coderyte <teamcoderyte@gmail.com>
2016-09-11 23:22:21 +0000

    Update Schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160831031457) do
+ActiveRecord::Schema.define(version: 20160911011654) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
